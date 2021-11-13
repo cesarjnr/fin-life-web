8b3df55ee9765b6e92afcf84206b7d020da6385f
César Júnior <cesar.jnr@hotmail.com>
2021-11-13 16:34:32 -0300

    Add toast for request errors

--- src/hooks/usePost.ts
+++ src/hooks/usePost.ts
@@ -1,14 +1,15 @@
 import axios from "axios";
 import { useState } from "react";
+import { useToast } from "@chakra-ui/toast";
 
 const api = axios.create({ baseURL: process.env.REACT_APP_API_BASE_URL });
 
 export const usePost = <T>(
   path: string
-): [<U>(payload: U) => void, boolean, string, T | undefined] => {
+): [<U>(payload: U) => void, boolean, T | undefined] => {
+  const toast = useToast();
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [createdResource, setCreatedResource] = useState<T>();
-  const [requestErrorMessage, setRequestErrorMessage] = useState<string>("");
   const makePostRequest = async <U>(payload: U) => {
     try {
       setIsLoading(true);
@@ -17,11 +18,17 @@ export const usePost = <T>(
 
       setCreatedResource(response.data);
     } catch (error: any) {
-      setRequestErrorMessage(error.message);
+      if (error.response) {
+        toast({
+          title: error.response.data.message,
+          status: 'error',
+          position: 'bottom-right'
+        });
+      }
     } finally {
       setIsLoading(false);
     }
   };
 
-  return [makePostRequest, isLoading, requestErrorMessage, createdResource];
+  return [makePostRequest, isLoading, createdResource];
 };
