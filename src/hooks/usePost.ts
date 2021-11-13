101c3a3ddd1c7bde59948a59a76b0ead54b50276
César Júnior <cesar.jnr@hotmail.com>
2021-11-13 15:43:50 -0300

    Fix env vars

--- src/hooks/usePost.ts
+++ src/hooks/usePost.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useState } from "react";
 
-const api = axios.create({ baseURL: process.env.API_BASE_URL });
+const api = axios.create({ baseURL: process.env.REACT_APP_API_BASE_URL });
 
 export const usePost = <T>(
   path: string
